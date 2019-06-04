6b3fc1b39135acaa099c26a108756c8d47dc58ba
macbookair <macbookair@GLM1076892.localdomain>
2019-06-05 02:24:04 +0800

    Created separate NavContainer component for better separationg of concerns

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from "react";
 import { Platform, StatusBar, Text, View } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
 import { Provider } from "react-redux";
-import Home from "./components/Home";
-import Splash from "./components/Splash";
+import NavContainer from "./components/NavContainer";
 import store from "./store";
 import colors from "./colors";
 
@@ -31,29 +21,12 @@ const MyStatusBar = ({ backgroundColor, ...props }) => (
   </View>
 );
 
-const Navigator = createStackNavigator({
-  Splash: {
-    screen: Splash,
-    navigationOptions: {
-      header: null
-    }
-  },
-  Home: {
-    screen: Home,
-    navigationOptions: {
-      header: null
-    }
-  }
-});
-
-const Container = createAppContainer(Navigator);
-
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <MyStatusBar />
-        <Container />
+        <NavContainer />
       </Provider>
     );
   }
