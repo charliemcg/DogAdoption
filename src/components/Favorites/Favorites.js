34008f4c0d10b99d782cae19c6dde33f20d80f6f
Charlie McGregor <charlie_mcgregor@outlook.com>
2019-06-12 13:13:43 +0800

    Styled favorites screen. It now has a title and will inform user there are no favorites if there are none. If there are no favorites then recently viewed dogs will appear att the bottom of the screen.

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
-import { SafeAreaView, Text, FlatList, ImageBackground } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  FlatList,
+  ImageBackground,
+  View
+} from "react-native";
 import { connect } from "react-redux";
 import styles from "./styles";
 import colors from "../../colors";
 import MiniListItem from "../MiniListItem";
 import backgroundImg from "../../images/backgroundWhite.png";
+import RecentlyViewed from "../RecentlyViewed";
 
 class Favorites extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -16,24 +23,54 @@ class Favorites extends Component {
   };
 
   render() {
+    // list of favorites
+    const thereAreFavorites = (
+      <FlatList
+        style={styles.list}
+        data={this.props.favorites}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <MiniListItem
+            item={item}
+            navigation={this.props.navigation}
+            showFav={true}
+          />
+        )}
+      />
+    );
+
+    // recently viewed dogs
+    const recents = (
+      <View style={styles.recents}>
+        <RecentlyViewed navigation={this.props.navigation} showFav={false} />
+      </View>
+    );
+
+    // inform user there are no favorites
+    const thereAreNoFavorites = (
+      <View style={styles.noFavoritesWrapper}>
+        <View style={styles.noFavorites}>
+          <Text style={styles.noFavoritesText}>You have no favorites...</Text>
+        </View>
+        {/* check there are recents to view */}
+        {this.props.recentlyViewed.length > 1 && recents}
+      </View>
+    );
+
     return (
       <ImageBackground
-        style={styles.parent}
+        style={{ flex: 1 }}
         resizeMode="cover"
         source={backgroundImg}
       >
         <SafeAreaView style={styles.parent}>
-          <FlatList
-            data={this.props.favorites}
-            numColumns={2}
-            renderItem={({ item }) => (
-              <MiniListItem
-                item={item}
-                navigation={this.props.navigation}
-                showFav={true}
-              />
-            )}
-          />
+          <View style={styles.titleWrapper}>
+            <Text style={styles.title}>Your Favorites</Text>
+          </View>
+          {/* check if favorites exist */}
+          {this.props.favorites.length === 0
+            ? thereAreNoFavorites
+            : thereAreFavorites}
         </SafeAreaView>
       </ImageBackground>
     );
@@ -42,7 +79,8 @@ class Favorites extends Component {
 
 const mapStateToProps = state => {
   return {
-    favorites: state.favorites
+    favorites: state.favorites,
+    recentlyViewed: state.recentlyViewed
   };
 };
 
--- src/components/Favorites/styles/phone.js
+++ src/components/Favorites/styles/phone.js
@@ -12,5 +12,30 @@ export default {
     marginRight: width * 0.02,
     marginTop: width * 0.01,
     marginBottom: width * 0.01
+  },
+  titleWrapper: {
+    width,
+    height: "10%",
+    justifyContent: "center",
+    paddingLeft: "7%"
+  },
+  title: {
+    fontSize: 30,
+    color: colors.fadedText
+  },
+  // favorites exist
+  list: { width: "100%", height: "90%" },
+  // favorites don't exist
+  noFavoritesWrapper: { height: "90%", width },
+  noFavorites: {
+    width: "100%",
+    height: "70%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  noFavoritesText: { fontSize: 20, color: colors.fadedText },
+  recents: {
+    height: "30%",
+    width
   }
 };
